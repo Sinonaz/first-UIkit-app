8b46d1134de9778db42613737a52d14eaa01d0d9
Isaac <aziksimpson@gmail.com>
2026-05-15 20:51:40 +0300

    Initial Commit

---
+++ firstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstApp
+//
+//  Created by Isaac Shir on 15/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
