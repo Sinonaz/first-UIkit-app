5b555634df73b349d7718f4833fcd5b57afb1a4c
Isaac <aziksimpson@gmail.com>
2026-05-15 22:12:05 +0300

    First storiboard with counter

--- firstApp/ViewController.swift
+++ firstApp/ViewController.swift
@@ -12,8 +12,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.updateCountLabel()
     }
-
-
+    
+    var countValue: Int = 0
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    func updateCountLabel() {
+        self.countLabel.text = "\(self.countValue)"
+    }
+    
+    @IBAction func decreaseBtnDidTap(_ sender: Any) {
+        self.countValue -= 1
+        self.updateCountLabel()
+    }
+    
+    @IBAction func increaseBtnDidTap(_ sender: Any) {
+        self.countValue += 1
+        self.updateCountLabel()
+    }
 }
-
